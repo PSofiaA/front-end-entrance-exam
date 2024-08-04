ade967694cb33e70674dd9abcadf5d7e9d53831b
Sofia <smtn20@gmail.com>
2024-08-04 20:21:10 +0300

    feat: add download functionality

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
 import "../css/style.css";
-// import javascriptLogo from '../javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+function main() {
+  const downloadBtn = document.getElementById("downloadBtn");
 
-// setupCounter(document.querySelector('#counter'))
+  downloadBtn.addEventListener("click", function () {
+    console.log("clicked");
+    const cvContent = document.getElementById("bentoContainer");
+    var opt = {
+      margin: 1,
+      filename: "CV.pdf",
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    };
+    html2pdf().set(opt).from(cvContent).save();
+  });
+}
+main();
